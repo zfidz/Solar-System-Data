a93a359802e529a9542396aa57ad84fd8b85a1e3
Zack Fiddler <zack.fiddler@me.com>
2022-12-02 01:23:22 -0800

    API functional

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
@@ -7,6 +7,17 @@ import './App.css';
 
 function App() {
   const [user, setUser] = useState(getUser());
+    const [bodyState, setBodyState] = useState([]);
+    useEffect(function () {
+      async function getBodies() {
+        const allBodies = await fetch("https://api.le-systeme-solaire.net/rest/bodies").then(
+          (res) => res.json()
+        );
+        setBodyState(allBodies);
+      }
+      getBodies();
+    },[]);
+
 
   return (
 <h1>test</h1>
