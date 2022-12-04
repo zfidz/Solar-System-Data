edf44f96e01830ac647e3ffdf5ca98366ed622b5
Zack Fiddler <zack.fiddler@me.com>
2022-12-04 00:23:58 -0800

    fix detail page (typo)

--- src/pages/PlanetDetailPage/PlanetDetailPage.jsx
+++ src/pages/PlanetDetailPage/PlanetDetailPage.jsx
@@ -4,7 +4,8 @@ export default function PlanetsPage({bodies}) {
 const planetName = useParams()
   return (
     <>
-{bodies.filter(planet => planet.englighName === planetName.planetName).map((planet, index) => (
+{bodies.filter(planet => planet.englishName === planetName.planetName).map((planet, index) => (
+ 
   <div key={index}>
     <h1>{planet.englishName}</h1>
   </div>
