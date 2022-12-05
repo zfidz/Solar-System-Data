8d11461099c4661ca93d8f0b73ae4dc2fa59039d
Zack Fiddler <zack.fiddler@me.com>
2022-12-05 10:13:09 -0800

    add bootstrap

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,44 +1,78 @@
-import { useState } from 'react';
-import * as usersService from '../../utilities/users-service';
+import { useState } from "react";
+import * as usersService from "../../utilities/users-service";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 export default function LoginForm({ setUser }) {
   const [credentials, setCredentials] = useState({
-    email: '',
-    password: ''
+    email: "",
+    password: "",
   });
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   function handleChange(evt) {
     setCredentials({ ...credentials, [evt.target.name]: evt.target.value });
-    setError('');
+    setError("");
   }
 
   async function handleSubmit(evt) {
     // Prevent form from being submitted to the server
     evt.preventDefault();
     try {
-      // The promise returned by the signUp service method 
+      // The promise returned by the signUp service method
       // will resolve to the user object included in the
       // payload of the JSON Web Token (JWT)
       const user = await usersService.login(credentials);
       setUser(user);
     } catch {
-      setError('Log In Failed - Try Again');
+      setError("Log In Failed - Try Again");
     }
   }
 
   return (
     <div>
       <div className="form-container">
-        <form autoComplete="off" onSubmit={handleSubmit}>
-          <label>Email</label>
-          <input type="text" name="email" value={credentials.email} onChange={handleChange} required />
-          <label>Password</label>
-          <input type="password" name="password" value={credentials.password} onChange={handleChange} required />
-          <button type="submit">LOG IN</button>
-        </form>
+        <Form autoComplete="off" onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              value={credentials.email}
+              onChange={handleChange}
+              required
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={credentials.password}
+              onChange={handleChange}
+              required
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
       </div>
       <p className="error-message">&nbsp;{error}</p>
     </div>
   );
 }
+
+//     <form autoComplete="off" onSubmit={handleSubmit}>
+//       <label>Email</label>
+//       <input type="text" name="email" value={credentials.email} onChange={handleChange} required />
+//       <label>Password</label>
+//       <input type="password" name="password" value={credentials.password} onChange={handleChange} required />
+//       <button type="submit">LOG IN</button>
+//     </form>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,10 @@
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 export default function NavBar({ user, setUser }) {
 
@@ -11,29 +16,59 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <nav>
-      { user ?
-      <>
-      <Link to="">Planets</Link>
-      &nbsp; | &nbsp;
-      <Link to="/moons">Moons</Link>
-      &nbsp; | &nbsp;
-      <span>Welcome, {user.name}</span>
-      &nbsp; | &nbsp;
-      <Link to={`/${user.name}`}>Profile</Link>
-      &nbsp; | &nbsp;
-      <Link to="" onClick={handleLogOut}>Log Out</Link>
-   </>
-  : 
-  <>
-  <Link to="">Planets</Link>
-  &nbsp; | &nbsp;
-  <Link to="/moons">Moons</Link>
-  &nbsp; | &nbsp;
-  <Link to="/authpage">Login/Sign Up</Link>
-
-</>
-  }
-    </nav>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#deets">More deets</Nav.Link>
+          <Nav.Link eventKey={2} href="#memes">
+            Dank memes
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   );
 }
+
+//    <nav>
+//       { user ?
+//       <>
+//       <Link to="">Planets</Link>
+//       &nbsp; | &nbsp;
+//       <Link to="/moons">Moons</Link>
+//       &nbsp; | &nbsp;
+//       <span>Welcome, {user.name}</span>
+//       &nbsp; | &nbsp;
+//       <Link to={`/${user.name}`}>Profile</Link>
+//       &nbsp; | &nbsp;
+//       <Link to="" onClick={handleLogOut}>Log Out</Link>
+//    </>
+//   : 
+//   <>
+//   <Link to="">Planets</Link>
+//   &nbsp; | &nbsp;
+//   <Link to="/moons">Moons</Link>
+//   &nbsp; | &nbsp;
+//   <Link to="/authpage">Login/Sign Up</Link>
+
+// </>
+//   }
+//     </nav>
